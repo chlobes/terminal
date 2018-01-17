0b21a47d61ec2191519ca2ffea62fdc6931ac1f3
pypypypypypypypypyp <1michand1@gmail.com>
2018-01-18 03:13:45 +1100

    first

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+use std::sync::mpsc::{channel, Receiver};
+use std::thread;
+
+pub struct Terminal<T: 'static + Parse + std::marker::Send> {
+	recver: Receiver<T>,
+}
+
+impl <T: Parse + std::marker::Send> Terminal<T> {
+	pub fn start() -> Self {
+		let (sender, recver) = channel();
+		
+		thread::spawn(move|| {
+			let mut line = String::new();
+			
+			loop {
+				
+				
+				std::io::stdin().read_line(&mut line).expect("terminal failed to read line");
+				
+				if let Some(command) = T::parse(&line) {
+					if let Err(_) = sender.send(command) {
+						break
+					}
+				}
+			}
+		});
+		
+		Terminal {
+			recver: recver,
+		}
+	}
+	
+	pub fn next(&self) -> Option<T> {
+		if let Ok(x) = self.recver.try_recv() {
+			Some(x)
+		} else {
+			None
+		}
+	}
+}
+
+pub trait Parse {
+	fn parse(line: &String) -> Option<Self>
+	where
+		Self: std::marker::Sized;
+}
